0d8e4eb57cf7e22ab67c9bafdf5ab492545ac92b
Vicente Arce <vicente.arce.p@usach.cl>
2023-11-30 09:28:33 -0300

    Creando Comun User

---
+++ src/main/java/CodigoFuente_21538935_ArcePalacios/Models_21538935_ArcePalacios/ComunUser_21538935_ArcePalacios.java
@@ -0,0 +1,12 @@
+package CodigoFuente_21538935_ArcePalacios.Models_21538935_ArcePalacios;
+
+public class ComunUser_21538935_ArcePalacios extends AbstractUser_21538935_ArcePalacios{
+
+    /**
+     * Constructor de usuario comun (Setea false en isAdmin)
+     * @param username nombre del usuario
+     */
+    public ComunUser_21538935_ArcePalacios(String username) {
+        super(username, false);
+    }
+}
